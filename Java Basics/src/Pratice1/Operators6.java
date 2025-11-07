ea1290ef82f8bc17a1957c8d4904c7789fb6fc26
kotireddy10 <kotireddykummita10@gmail.com>
2025-11-07 19:28:24 +0530

    HI Good Evening

---
+++ Java Basics/src/Pratice1/Operators6.java
@@ -0,0 +1,10 @@
+package Pratice1;
+
+public class Operators6 {
+    public static void main(String args[]){
+        int apples=7;
+        int eaten=3;
+        int Left=apples-eaten;
+        System.out.println(Left);
+    }
+}
